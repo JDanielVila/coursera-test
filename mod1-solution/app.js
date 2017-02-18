6d0db42ce1675d865ff02e5e9f0b0fdca06d8f20
jDaniel <jdvilarguez@gmail.com>
2017-02-18 20:29:11 +0100

    First assigment solution

---
+++ mod1-solution/app.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController ($scope) {
+
+    $scope.lunchList = "";
+    $scope.lunchHint = "";
+
+    $scope.checkIfTooMuch = function () {
+      var num = getLunchItems($scope.lunchList);
+      if (num == 0) {
+        $scope.lunchHint = 'Please enter data first';
+      } else {
+        $scope.lunchHint = (num <= 3 ? 'Enjoy!' : 'Too much!');
+      }
+    };
+
+    // Empty items aren't taken in account
+    function getLunchItems(string) {
+      var trimSep = /^\s*\,*\s*|\s*\,*\s*$/g; // RegExp to discard begin or end separators
+      var separator = /\,+\s*\,*/; // Separator RegExp that discards empty items
+      string = string.replace(trimSep,"");
+      if(string.length > 0) {
+        return string.split(separator).length;
+      } else {
+        return 0;
+      }
+    }
+
+  }; //controller end
+
+})(); //funtion end
