1130c83da542935d8826416b229d023dae6a88a1
jDaniel <jdvilarguez@gmail.com>
2017-03-21 01:18:05 +0100

    Registration controller enhanced

--- mod5-solution/src/public/registration/registration.controller.js
+++ mod5-solution/src/public/registration/registration.controller.js
@@ -14,15 +14,20 @@ function RegistrationController(RegistrationService, MenuService) {
   this.user = RegistrationService.getRegistration();
 
   regCtrl.submit = function () {
-    regCtrl.saved = false;
-    regCtrl.dishError = false;
-    
     if (regCtrl.user.dish) {
       var promise = MenuService.isMenuItem(regCtrl.user.dish);     
-      promise.then(function(value) { regCtrl.dishError = !value });
-    } 
-
-    if (regCtrl.dishError == false) {
+      promise.then(function(value) { 
+        if(value) {
+          regCtrl.dishError == false;
+          RegistrationService.saveRegistration(regCtrl.user);
+          regCtrl.saved = true;
+        } else {
+          regCtrl.dishError = true;
+          regCtrl.saved = false;
+        }
+      });
+    } else {
+      regCtrl.dishError = false;             
       RegistrationService.saveRegistration(regCtrl.user);
       regCtrl.saved = true;
     } 
